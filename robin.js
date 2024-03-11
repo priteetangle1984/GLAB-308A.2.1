f6713361e98787bb0be191eac1c471c46a0b83c2
PriteeTangle <140731865+priteetangle1984@users.noreply.github.com>
2024-03-11 14:41:22 -0400

    "More changes"

--- robin.js
+++ robin.js
@@ -61,15 +61,17 @@ console.log("===================================================================
 
 
 class Character {
+    //add a static  MAX_HEALTH property to the Character class equal to 100.
+    //can only be accessed by the class itself by doing console.log(Character.MAX_HEALTH);
+    static MAX_HEALTH = 100;
     constructor(name) {
         this.name = name;
         this.health = 100;
         this.inventory = [];
     }
-
     roll(mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
-        console.log(`${this.name} rolled a ${result}.`)
+        console.log(`${this.name} rolled a ${result}.`);
     }
 }
 const robin = new Character("Robin");
@@ -79,36 +81,79 @@ robin.companion.type = "Cat";
 robin.companion.companion = new Character("Frank");
 robin.companion.companion.type = "Flea";
 robin.companion.companion.inventory = ["small hat", "sunglasses"];
+console.log(robin);
+console.log(robin.companion.type);
+// console.log(Character);
+robin.companion.companion.roll();
 
 // console.log(robin.inventory)
 // console.log(Character)
 
 
-robin.companion.companion.roll();
-robin.companion.roll();
-robin.roll();
+// robin.companion.companion.roll();
+// robin.companion.roll();
+// robin.roll();
 
+console.log("=============================================================================")
 // PART 3 - Class Features
 
 class Adventurer extends Character {
-    constructor (name, role) {
-      super(name);
-      // Adventurers have specialized roles.
-      this.role = role;
-      // Every adventurer starts with a bed and 50 gold coins.
-      this.inventory.push("bedroll", "50 gold coins");
+    // Add a static ROLES array to the Adventurer class, with the values “Fighter,” “Healer,” and “Wizard.” Feel free to add other roles, if you desire!
+    static ROLES = ['Fighter', 'Healer', 'Wizard'];
+    constructor(name, role,) {
+        super(name);
+        // Adventurers have specialized roles.
+        this.role = role;
+        // Every adventurer starts with a bed and 50 gold coins.
+        this.inventory.push("bedroll", "50 gold coins");
     }
     // Adventurers have the ability to scout ahead of them.
-    scout () {
-      console.log(`${this.name} is scouting ahead...`);
-      super.roll();
+    scout() {
+        console.log(`${this.name} is scouting ahead...`);
+        super.roll();
     }
-
-    // speak () {
-    //     console.log(`${this.name} says meow ....`)
-    // }
-  }
-
-  class Companion extends Character {
-    
-  }
+    discover(){
+        console.log(`${this.name} is discovering...`);
+        super.roll();
+    }
+    fish(){
+        console.log(`${this.name} is fishing...`);
+        super.roll();
+    }
+    addItems(items){
+    console.log(this.inventory.push(items));
+    }
+}
+console.log(Adventurer.ROLES);
+const Pritee = new Adventurer ('Pritee', 'girl scout');
+console.log(Pritee);
+//answer:
+// Adventurer {
+//     name: 'Pritee',
+//     health: 100,
+//     inventory: [ 'bedroll', '50 gold coins' ],
+//     role: 'girl scout'
+//   }
+Pritee.fish();
+//answer:
+// Pritee is fishing...
+// Pritee rolled a 11.
+//Next, create a Companion class with properties and methods specific to the companions.
+class Companion extends Character {
+    constructor(name, type){
+        super(name);
+        this.name = name;
+        this.type = type;
+    }
+    //methods
+    assist(){
+        console.log (`${this.name} is a ${this.type} assisting the adventurer.`)
+    }
+}
+const Tinkerbell = new Companion ("Tinkerbell", "fairy");
+Tinkerbell.assist();
+//answer: Tinkerbell is a fairy assisting the adventurer.
+// Finally, change the declaration of Robin and the companions to use the new Adventurer and Companion classes.
+let Robin = new Adventurer('Robin','adventurer');
+let Leo = new Companion('Leo', 'cat');
+let Frank = new Companion ('Frank', 'flea');
